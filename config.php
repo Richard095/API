5a705e246c0413c94ca53134f04c028a2ce1c704
Richard095 <ricardorm789@gmail.com>
2019-06-12 19:55:40 -0500

    More changes..

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 $db = [
-    'host' => 'mysql5017.site4now.net',
-    'username' => 'a49126_petdb',
-    'password' => '15690211rm789',
-    'db' => 'db_a49126_petdb'
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+    'db' => 'petdb'
 ];
 ?>
